2740415effebbbced3a66d70cbd056f7d0688573
smarie33 <shannon@mosleygraphics.com>
2023-04-20 16:56:08 -0400

    word border

--- wp-content/themes/whospeaksforme/template-parts/acf/acf-free_form_content.php
+++ wp-content/themes/whospeaksforme/template-parts/acf/acf-free_form_content.php
@@ -12,6 +12,15 @@
 
 ?>
 
-<section class="acf-free_form_content alignfull" style="background-color:<?php the_sub_field('background_color'); ?>;">
-  <div>  <?php the_sub_field('content'); ?></div>
+<section class="acf-free_form_content alignfull<?php if(get_sub_field('word_border')): ?> word-border<?php endif; ?>" style="background-color:<?php the_sub_field('background_color'); ?>;">
+  <?php if(get_sub_field('word_border')): ?>
+    <div class="top"><div class="words">who speaks for me?</div></div>
+    <div class="centered">
+      <div class="left"><div class="words">who speaks for me?</div></div>
+  <?php endif; ?>
+    <div class="content"><?php the_sub_field('content'); ?></div>
+  <?php if(get_sub_field('word_border')): ?>
+    <div class="right"><div class="words">who speaks for me?</div></div>
+    </div>
+  <?php endif; ?>
 </section>
