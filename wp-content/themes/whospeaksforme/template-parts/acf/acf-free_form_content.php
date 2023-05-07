c1003ed1ec13e00e1e5f33905e6a28dc0a931353
smarie33 <shannon@mosleygraphics.com>
2023-05-07 02:07:31 -0400

    updated ACF, add padding options to free form content and 2 column layout, add column width options to 2 column layout

--- wp-content/themes/whospeaksforme/template-parts/acf/acf-free_form_content.php
+++ wp-content/themes/whospeaksforme/template-parts/acf/acf-free_form_content.php
@@ -9,10 +9,36 @@
  * @since Twenty Twenty-One 1.0
  */
 
+$style = ' style="';
+$end = '"';
+$padding = '';
+$styles = '';
+
+$padding_array = [];
+$style_write = [];
+
+if(get_sub_field('padding_left') != '' || get_sub_field('padding_top') != '' || get_sub_field('padding_right') != '' || get_sub_field('padding_bottom') != ''){
+  $padding_array[] = (get_sub_field('padding_top') == '' || get_sub_field('padding_top') === 0) ? 0 : get_sub_field('padding_top').'px';
+  $padding_array[] = (get_sub_field('padding_right') == '' || get_sub_field('padding_right') === 0) ? 0 : get_sub_field('padding_right').'px';
+  $padding_array[] = (get_sub_field('padding_bottom') == '' || get_sub_field('padding_bottom') === 0) ? 0 : get_sub_field('padding_bottom').'px';
+  $padding_array[] = (get_sub_field('padding_left') == '' || get_sub_field('padding_left') === 0) ? 0 : get_sub_field('padding_left').'px';
+  $padding = implode(' ',$padding_array);
+  if($padding != '0 0 0 0'){
+    $style_write[] = 'padding: '.$padding_left_write;
+  }
+}
+
+if(get_sub_field('background_color') != ''){
+  $style_write[] = 'background-color:'.get_sub_field('background_color');
+}
+
+if(count($style_write) > 0){
+  $styles = $style.implode('; ',$style_write).$end;
+}
 
 ?>
 
-<section class="acf-free_form_content alignfull<?php if(get_sub_field('word_border')): ?> word-border<?php endif; ?>" style="background-color:<?php the_sub_field('background_color'); ?>;">
+<section class="acf-free_form_content alignfull<?php if(get_sub_field('word_border')): ?> word-border<?php endif; ?>"<?php echo $styles ?>>
   <?php if(get_sub_field('word_border')): ?>
     <div class="top"><div class="words">who speaks for me?</div></div>
     <div class="centered">
--- wp-content/themes/whospeaksforme/template-parts/acf/acf-two_columns.php
+++ wp-content/themes/whospeaksforme/template-parts/acf/acf-two_columns.php
@@ -9,11 +9,62 @@
  * @since Twenty Twenty-One 1.0
  */
 
+$style = ' style="';
+$end = '"';
+$padding_left_side = '';
+$styles_left = '';
+
+$padding_right_side = '';
+$styles_right = '';
+
+$columnSize = explode('-',get_sub_field('column_sizes'));
+$padding_array_left = [];
+$style_write_left = [];
+$style_write_left[] = 'width:'.$columnSize[0];
+
+if(get_sub_field('padding_left_left') != '' || get_sub_field('padding_top_left') != '' || get_sub_field('padding_right_left') != '' || get_sub_field('padding_bottom_left') != ''){
+  $padding_array_left[] = get_sub_field('padding_top_left') == '' ? 0 : get_sub_field('padding_top_left').'px';
+  $padding_array_left[] = get_sub_field('padding_right_left') == '' ? 0 : get_sub_field('padding_right_left').'px';
+  $padding_array_left[] = get_sub_field('padding_bottom_left') == '' ? 0 : get_sub_field('padding_bottom_left').'px';
+  $padding_array_left[] = get_sub_field('padding_left_left') == '' ? 0 : get_sub_field('padding_left_left').'px';
+  $padding_left_write = implode(' ',$padding_array_left);
+  if($padding_left_write != '0 0 0 0'){
+    $style_write_left[] = 'padding: '.$padding_left_write;
+  }
+}
+
+if(get_sub_field('background_color_left') != ''){
+  $style_write_left[] = 'background-color:'.get_sub_field('background_color_left');
+}
+
+$styles_left = $style.implode('; ',$style_write_left).$end;
+
+$padding_array_right = [];
+$style_write_right = [];
+$style_write_right[] = 'width:'.$columnSize[1];
+
+if(get_sub_field('padding_left_right') != '' || get_sub_field('padding_top_right') != '' || get_sub_field('padding_right_right') != '' || get_sub_field('padding_bottom_right') != ''){
+  $padding_array_right[] = get_sub_field('padding_left_right') == '' ? 0 : get_sub_field('padding_left_right').'px';
+  $padding_array_right[] = get_sub_field('padding_top_right') == '' ? 0 : get_sub_field('padding_top_right').'px';
+  $padding_array_right[] = get_sub_field('padding_right_right') == '' ? 0 : get_sub_field('padding_right_right').'px';
+  $padding_array_right[] = get_sub_field('padding_bottom_right') == '' ? 0 : get_sub_field('padding_bottom_right').'px';
+  $padding_right_write = implode(' ',$padding_array_right);
+  if($padding_right_write != '0 0 0 0'){
+    $style_write_right[] = 'padding: '.$padding_right_write;
+  }
+}
+
+if(get_sub_field('background_color_right') != ''){
+  $style_write_right[] = 'background-color:'.get_sub_field('background_color_right');
+}
+
+$styles_right = $style.implode('; ',$style_write_right).$end;
+
 
 ?>
 
 <section class="acf-two_columns alignfull">
-  <div class="column<?php if(get_sub_field('full_width_images_left')): ?> full-width-images<?php endif; ?><?php if(get_sub_field('add_button_left')): ?> with-button<?php endif; ?>" style="background-color: <?php the_sub_field('background_color_left'); ?>;">
+  <div class="column<?php if(get_sub_field('full_width_images_left')): ?> full-width-images<?php endif; ?><?php if(get_sub_field('add_button_left')): ?> with-button<?php endif; ?>"<?php echo $styles_left; ?>>
     <?php the_sub_field('content_left'); ?>
         <?php if(get_sub_field('add_button_left')): ?>
       <?php $open_new = get_field('link_left')['target'] ? get_field('link_left')['target'] : '_self'; ?>
@@ -22,7 +73,7 @@
       </p>
     <?php endif; ?>
   </div>
-  <div class="column<?php if(get_sub_field('full_width_images_right')): ?> full-width-images<?php endif; ?><?php if(get_sub_field('add_button_right')): ?> with-button<?php endif; ?>" style="background-color: <?php the_sub_field('background_color_right'); ?>;">
+  <div class="column<?php if(get_sub_field('full_width_images_right')): ?> full-width-images<?php endif; ?><?php if(get_sub_field('add_button_right')): ?> with-button<?php endif; ?>"<?php echo $styles_right; ?>>
     <?php the_sub_field('content_right'); ?>
     <?php if(get_sub_field('add_button_right')): ?>
       <?php $open_new = get_field('link_right')['target'] ? get_field('link_right')['target'] : '_self'; ?>
