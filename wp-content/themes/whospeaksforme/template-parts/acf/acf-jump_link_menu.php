96e99db7a650081a2696c93fe42bb7a283bee2ec
smarie33 <shannon@mosleygraphics.com>
2023-05-30 06:01:00 -0400

    responsive jumplink nav, no nav on mobile

---
+++ wp-content/themes/whospeaksforme/template-parts/acf/acf-jump_link_menu.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template part for displaying an advanced custom field
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Who Speaks For Me
+ * @since Twenty Twenty-One 1.0
+ */
+
+
+$jump_nav = get_sub_field('jump_link_navigation');
+$modified_titles = [];
+$title_index = 0;
+?>
+
+<nav class="acf-jump-link-nav alignfull">
+    <ul>
+    <?php foreach($jump_nav as $nav_item):
+        $collapsed_title = str_replace(" ", "-", strtolower($nav_item['jump_title']));
+        $modified_titles[] = $collapsed_title;
+    ?>
+        <li><a class="jump-link" href="#<?php echo $collapsed_title; ?>"><span><?php echo $nav_item['jump_title']; ?></span></a></li>
+    <?php endforeach; ?>
+    </ul>
+    <hr class="double-border">
+</nav>
